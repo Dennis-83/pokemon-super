5c782ef9441ed35e1094776fd129a934dad7caed
Dennis-83 <dodo_ez@hotmail.com>
2025-03-29 15:20:12 +0100

    Added Pokemon super class

---
+++ src/Pokemon.java
@@ -0,0 +1,56 @@
+public abstract class Pokemon {
+    private final String name;
+    private int level;
+    private int hp;
+    private int xp;
+    private final String sound;
+
+    // Constructor
+
+    public Pokemon(String name, String sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    // Getters and Setters (no setters for name,sound)
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    // Methods
+
+    public abstract void attack();
+
+    public void makeSound() {
+        System.out.println(name + " shouts:" + sound + "!");
+    }
+}
